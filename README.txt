e509830d1ab848a04c5c412044dc234c561fc437
Charisma-Ricarte <ann.marie.chr29@gmail.com>
2025-09-16 13:26:48 -0500

    wip update Co-authored-by: nerevar <nere-var@users.noreply.github.com>

--- README.txt
+++ README.txt
@@ -1,3 +1,7 @@
 To run server :
         python chat_server.py <listen_port>
-To run client 
+To run client :
+        python chat_client.py <server_ip> <server_port>
+Ex:
+        python chat_server.py 5000
+        python chat_client.py 127.0.0.1 5000
--- chat_server.py
+++ chat_server.py
@@ -25,4 +25,12 @@ def handle_client(conn):
 
 def main():
     if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} listen_port")
+        sys.exit(1)
+        
+        listen_port = int(sys.argv[1])
+
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind(("0.0.0.0", listen_port))
+        server_socket.listen(1)
         
